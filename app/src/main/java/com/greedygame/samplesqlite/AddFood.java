3ff47d9ee1140e917e3b8f3833e4631bb4b59cd5
amitranjan <amitranjan036@gmail.com>
2017-04-16 23:14:30 +0530

    2nd activity is added

--- app/src/main/java/com/greedygame/samplesqlite/AddFood.java
+++ app/src/main/java/com/greedygame/samplesqlite/AddFood.java
@@ -1,7 +1,9 @@
 package com.greedygame.samplesqlite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,20 +13,44 @@ import android.widget.EditText;
 
 public class AddFood extends AppCompatActivity{
 
+    private View.OnClickListener buttonListener;
+    DatabaseHandler myDb;
+    Intent in;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_add);
 
-        EditText foodName = (EditText) findViewById(R.id.foodName);
+        myDb = new DatabaseHandler(this);
+
+        final EditText foodName = (EditText) findViewById(R.id.foodName);
         Button okButton = (Button) findViewById(R.id.button1);
         Button cancelButton = (Button) findViewById(R.id.button2);
 
+        buttonListener = new View.OnClickListener(){
 
+        @Override
+        public void onClick(View v)
+        {
+            switch(v.getId())
+            {
+                case R.id.button2:
+                in = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(in);
+                    break;
 
+                case R.id.button1:
+                    myDb.insertData(foodName.getText().toString());
+                    in = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(in);
+                    break;
+            }
+        }
+    };
 
-
-
+        cancelButton.setOnClickListener(buttonListener);
+        okButton.setOnClickListener(buttonListener);
 }
 }
--- app/src/main/java/com/greedygame/samplesqlite/MainActivity.java
+++ app/src/main/java/com/greedygame/samplesqlite/MainActivity.java
@@ -1,33 +1,21 @@
 package com.greedygame.samplesqlite;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
-
-import java.util.ArrayList;
-
-import static android.R.id.input;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHandler myDb;
     ListView list;
     ArrayAdapter<String> adapter;
-    private String m_Text = "";
     public FloatingActionButton plus;
-    private Context context;
-    private AlertDialog builder;
+   // private Context context;
 
-    // ArrayList<Food> foodArrayList = new ArrayList<Food>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -38,11 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
         list= (ListView) findViewById(R.id.screenlist);
         plus= (FloatingActionButton) findViewById(R.id.fab);
-        builder = new AlertDialog.Builder()
-        EditText input = new EditText(context);
-//        input.setInputType(InputType.TYPE_CLASS_TEXT );
+     //   EditText input = new EditText(context);
 
-        context = this;
+      //  context = this;
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
         adapter.addAll(myDb.getAllFood());
         list.setAdapter(adapter);
@@ -51,25 +37,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-
-                builder.setTitle("Add Food Name");
-
-
-//            builder = new AlertDialog.Builder(context).setTitle("Add Food Name").setView().setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    m_Text = input.getText().toString();
-//                }
-//            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    dialog.cancel();
-//                }
-//            }).show();
-
+                Intent intent = new Intent(getApplicationContext(), AddFood.class);
+                startActivity(intent);
             }
         });
-
         }
 
     }
